faa6dd0e2151c4b83b8b2c66182aee2f9c644eab
CRedmon-F360 <redmoncoreyl@gmail.com>
2021-04-05 14:49:20 -0400

    Edge Class Added This class reperesents an edge in a graph, i.e. it has two vertices represented by two integers.

---
+++ Edge.cpp
@@ -0,0 +1,73 @@
+#include "Edge.hpp"
+
+// default constructor
+Edge::Edge() : v_a(-1), v_b(-1) { }
+
+// standard constructor
+Edge::Edge(Vertex a, Vertex b) : v_a(a), v_b(b), id(0) { }
+
+// standard constructor
+Edge::Edge(Vertex a, Vertex b, int i) : v_a(a), v_b(b), id(i) { }
+
+// copy constructor
+Edge::Edge(const Edge& e) : v_a(e.v_a), v_b(e.v_b), id(e.id) { }
+
+// swap function
+void swap(Edge& e1, Edge& e2) {
+    std::swap(e1.v_a, e2.v_a);
+    std::swap(e1.v_b, e2.v_b);
+    std::swap(e1.id, e2.id);
+}
+
+// copy assignment operator
+Edge& Edge::operator = (Edge e) {
+    swap(*this, e);
+    return *this;
+}
+
+// destructor
+Edge::~Edge() { }
+
+// stream insertion operator
+std::ostream& operator << (std::ostream& out, const Edge& e) {
+    out << e.v_a << " " << e.v_b;
+    return out;
+}
+
+// compare operator
+bool operator < (const Edge& lhs, const Edge& rhs) {
+    return lhs.id < rhs.id;
+}
+
+// compare operator
+bool operator > (const Edge& lhs, const Edge& rhs) {
+    return lhs.id > rhs.id;
+}
+
+// compare operator
+bool operator <= (const Edge& lhs, const Edge& rhs) {
+    return lhs.id <= rhs.id;
+}
+
+// compare operator
+bool operator >= (const Edge& lhs, const Edge& rhs) {
+    return lhs.id >= rhs.id;
+}
+
+// compare operator
+bool operator == (const Edge& lhs, const Edge& rhs) {
+    return lhs.id == rhs.id;
+}
+
+// tests if edge connects to given vertex
+bool Edge::isIncident(Vertex a) {
+    return (v_a == a || v_b == a);
+}
+
+Vertex Edge::getA() const {
+    return v_a;
+}
+
+Vertex Edge::getB() const {
+    return v_b;
+}
---
+++ Edge.hpp
@@ -0,0 +1,32 @@
+#ifndef EDGE_H
+#define EDGE_H
+
+#include <iostream>
+
+typedef int Vertex;
+
+class Edge {
+private:
+    Vertex v_a;
+    Vertex v_b;
+    int id;
+public:
+    Edge(); // default constructor
+    Edge(Vertex a, Vertex b); // standard constructor
+    Edge(Vertex a, Vertex b, int i); // standard constructor
+    Edge(const Edge& e); // copy constructor
+    friend void swap(Edge& e1, Edge& e2); // swap function
+    Edge& operator = (Edge e); // copy assignment operator
+    ~Edge(); // destructor
+    friend std::ostream& operator << (std::ostream& out, const Edge& e); // stream insertion operator
+    friend bool operator < (const Edge& lhs, const Edge& rhs); // compare operator
+    friend bool operator > (const Edge& lhs, const Edge& rhs); // compare operator
+    friend bool operator <= (const Edge& lhs, const Edge& rhs); // compare operator
+    friend bool operator >= (const Edge& lhs, const Edge& rhs); // compare operator
+    friend bool operator == (const Edge& lhs, const Edge& rhs); // compare operator
+    bool isIncident(Vertex a);
+    Vertex getA() const;
+    Vertex getB() const;
+};
+
+#endif
