e0555633848f8d91c7577c689501042f17901b29
CRedmon-F360 <redmoncoreyl@gmail.com>
2021-04-05 14:54:15 -0400

    Added graph class This class represents a graph. It will contain a collection of vertices, edges, and other helpful data.

---
+++ Graph.cpp
@@ -0,0 +1,60 @@
+#include "Graph.hpp"
+
+// default constructor
+Graph::Graph() { }
+
+// standard constructor
+Graph::Graph(std::istream& in) {
+    // read number of vertices and edges
+    in >> n >> m;
+
+    // read edges
+    for (int i = 0; i < m; i++) {
+        Vertex v1, v2;
+        in >> v1 >> v2;
+        edgeSet.insert(Edge(v1, v2, i));
+        vertexSet.insert(v1);
+        vertexSet.insert(v2);
+        adjacencyList[v1].insert(v2);
+        adjacencyList[v2].insert(v1);
+    }
+}
+
+// copy constructor
+Graph::Graph(const Graph& g) : n(g.n), m(g.m), edgeSet(g.edgeSet),
+    vertexSet(g.vertexSet), adjacencyList(g.adjacencyList) { }
+
+// swap function
+void swap(Graph& g1, Graph& g2) {
+    std::swap(g1.n, g2.n);
+    std::swap(g1.m, g2.m);
+    std::swap(g1.edgeSet, g2.edgeSet);
+    std::swap(g1.vertexSet, g2.vertexSet);
+    std::swap(g1.adjacencyList, g2.adjacencyList);
+}
+
+// copy assignment operator
+Graph& Graph::operator = (Graph g) {
+    swap(*this, g);
+    return *this;
+}
+
+// destructor
+Graph::~Graph() { }
+
+// stream insertion operator
+std::ostream& operator << (std::ostream& out, const Graph& g) {
+    out << g.n << " " << g.m << std::endl;
+    for (auto e : g.edgeSet) {
+        out << e << std::endl;
+    }
+    return out;
+}
+
+std::set<Edge> Graph::getEdges() {
+    return edgeSet;
+}
+
+std::set<Vertex> Graph::getVertices() {
+    return vertexSet;
+}
---
+++ Graph.hpp
@@ -0,0 +1,29 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "Edge.hpp"
+#include <vector>
+#include <map>
+#include <set>
+#include <iostream>
+
+class Graph {
+private:
+    int n; // number of vertices
+    int m; // number of edges
+    std::set<Edge> edgeSet;
+    std::set<Vertex> vertexSet;
+    std::map<Vertex, std::set<Vertex> > adjacencyList;
+public:
+    Graph(); // default constructor
+    Graph(std::istream& in); // standard constructor
+    Graph(const Graph& g); // copy constructor
+    friend void swap(Graph& g1, Graph& g2); // swap function
+    Graph& operator = (Graph g); // copy assignment operator
+    ~Graph(); // destructor
+    friend std::ostream& operator << (std::ostream& out, const Graph& g); // stream insertion operator
+    std::set<Edge> getEdges();
+    std::set<Vertex> getVertices();
+};
+
+#endif
