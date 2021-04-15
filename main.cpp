ed55c63a8a8596b087509c613cd6cccc1b937050
Corey Redmon <redmoncoreyl@gmail.com>
2021-04-15 09:27:43 -0400

    Added basic outline of main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include "Edge.hpp"
+#include "Slitherlink.hpp"
+#include "Zdd.hpp"
 
-int main() {
+template <typename T>
+void printFamily(Family<T>& f) {
+    std::cout << "[\n";
+    for (auto s : f) {
+        std::cout << "  {";
+        for (auto e : s) {
+            std::cout << e << ", ";
+        }
+        std::cout << "}\n";
+    }
+    std::cout << "]" << std::endl;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "Expected <exe> <slitherlink_file>" << std::endl;
+        exit(1);
+    }
+    std::ifstream slitherlink_in;
+    slitherlink_in.open(argv[1]);
+    // Family<Edge> f = s.generateFamily();
+    // printFamily(f);
 
     return 0;
 }
